709f0f6eba16726b6cf6d19c7b87b63110e23d8d
Adam J. Arling <aarling@gmail.com>
2023-02-25 15:50:37 -0600

    Add custom fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Nova_Mono, Lato } from "next/font/google";
+
+const novaMono = Nova_Mono({
+  subsets: ["latin"],
+  variable: "--font-nova-mono",
+  weight: "400",
+});
+const lato = Lato({
+  subsets: ["latin"],
+  variable: "--font-lato",
+  weight: "300",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-lato: ${lato.style.fontFamily};
+            --font-nova-mono: ${novaMono.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
