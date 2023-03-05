e652169b260732707a8913ee444975bb47afdcc0
Adam J. Arling <aarling@gmail.com>
2023-03-05 17:14:09 -0600

    Pull in new Masonry component files

--- app/process/page.tsx
+++ app/process/page.tsx
@@ -1,39 +1,37 @@
 import { promises as fs } from "fs";
 import path from "path";
+import { getPlaiceholder, type IGetPlaiceholderReturn } from "plaiceholder";
 
-import MasonryGallery, { type MasonryImage } from "@/components/Masonry";
+import MasonryGallery from "@/components/Masonry";
 const sizeOf = require("image-size");
 
-const GalleryPage = async () => {
-  const imageDirectory = path.join(process.cwd(), "/public/student-process");
+const folder = "student-process";
+
+const StudentProcessPage = async () => {
+  // Get filenames from a directory
+  const imageDirectory = path.join(process.cwd(), `/public/${folder}`);
   const imageFilenames = await fs.readdir(imageDirectory);
-  const images = imageFilenames.map((ifn): MasonryImage => {
-    const dimensions = sizeOf(`${imageDirectory}/${ifn}`);
-    return {
-      filename: ifn,
-      ...dimensions,
-    };
-  });
+
+  // Generate a blur loading image and image sizing
+  const promises: any[] = [];
+  imageFilenames.forEach((ifn) =>
+    promises.push(getPlaiceholder(`/${folder}/${ifn}`))
+  );
+  const responses = await Promise.all<IGetPlaiceholderReturn>(promises);
+  const images = responses.map((response) => ({
+    ...response.img,
+    placeholder: "blur",
+    blurDataURL: response.base64,
+  }));
 
   return (
     <>
       <section className="max-w-3xl py-8 mx-auto text-center">
-        {/* My role goes beyond simply imparting knowledge to my students.
-        For many, it's an opportunity to inspire and empower the next generation
-        of leaders, thinkers, and artists. For one Chicago Public Schools high
-        school teacher, art is not just a passion but also an integral part of
-        their classroom curriculum. This teacher's unique approach to
-        integrating their own creative process into the classroom has not only
-        fostered a love for the arts in their students but has also allowed them
-        to express themselves in ways they never thought possible. In this
-        article, we'll take a closer look at this teacher's process of making
-        art in the classroom and how it has positively impacted their students'
-        lives. */}
         Something goes here
       </section>
-      <MasonryGallery dir="student-process" images={images} />
+      {images && <MasonryGallery dir={folder} images={images} />}
     </>
   );
 };
 
-export default GalleryPage;
+export default StudentProcessPage;
--- app/student-artwork/page.tsx
+++ app/student-artwork/page.tsx
@@ -1,28 +1,37 @@
 import { promises as fs } from "fs";
 import path from "path";
+import { getPlaiceholder, type IGetPlaiceholderReturn } from "plaiceholder";
 
-import MasonryGallery, { MasonryImage } from "@/components/Masonry";
+import MasonryGallery from "@/components/Masonry";
 const sizeOf = require("image-size");
 
-const GalleryPage = async () => {
-  const imageDirectory = path.join(process.cwd(), "/public/student-artwork");
+const folder = "student-artwork";
+
+const StudentArtworkPage = async () => {
+  // Get filenames from a directory
+  const imageDirectory = path.join(process.cwd(), `/public/${folder}`);
   const imageFilenames = await fs.readdir(imageDirectory);
-  const images = imageFilenames.map((ifn): MasonryImage => {
-    const dimensions = sizeOf(`${imageDirectory}/${ifn}`);
-    return {
-      filename: ifn,
-      ...dimensions,
-    };
-  });
+
+  // Generate a blur loading image and image sizing
+  const promises: any[] = [];
+  imageFilenames.forEach((ifn) =>
+    promises.push(getPlaiceholder(`/${folder}/${ifn}`))
+  );
+  const responses = await Promise.all<IGetPlaiceholderReturn>(promises);
+  const images = responses.map((response) => ({
+    ...response.img,
+    placeholder: "blur",
+    blurDataURL: response.base64,
+  }));
 
   return (
     <>
       <section className="max-w-3xl py-8 mx-auto text-center">
         Something goes here
       </section>
-      <MasonryGallery dir="student-artwork" images={images} />
+      {images && <MasonryGallery dir={folder} images={images} />}
     </>
   );
 };
 
-export default GalleryPage;
+export default StudentArtworkPage;
