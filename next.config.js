333ca7b3f29e08bbac77fb4b7a0f07c1a4275827
Adam J. Arling <aarling@gmail.com>
2026-02-01 19:13:25 +0100

    Update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
   },
+  reactStrictMode: true,
+  experimental: {},
 };
 
 module.exports = nextConfig;
